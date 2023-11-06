8c55ad4fe571f7e147a61a279cd178c8d697053d
TUANTRAN118 <tuan79956@gmail.com>
2023-11-06 15:30:05 +0700

    add layout

---
+++ app/src/main/java/vn/edu/dlu/ctk45/calories_app/MucTieu_ACT.java
@@ -0,0 +1,14 @@
+package vn.edu.dlu.ctk45.calories_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MucTieu_ACT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.muc_tieu);
+    }
+}
---
+++ app/src/main/java/vn/edu/dlu/ctk45/calories_app/ThongTinCaNhan_ACT.java
@@ -0,0 +1,14 @@
+package vn.edu.dlu.ctk45.calories_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongTinCaNhan_ACT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thong_tin_ca_nhan);
+    }
+}
